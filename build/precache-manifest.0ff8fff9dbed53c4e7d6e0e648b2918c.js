64db90293da1be677bc1fbfa65ff5727ee98fad7
MrtnOmwenga <martin36449@gmail.com>
2023-07-17 23:27:06 +0300

    Added build for the frontend and served it

---
+++ build/precache-manifest.0ff8fff9dbed53c4e7d6e0e648b2918c.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "210fb3cc36cc3ae17de5",
+    "url": "/static/css/main.7411cb2d.chunk.css"
+  },
+  {
+    "revision": "210fb3cc36cc3ae17de5",
+    "url": "/static/js/main.210fb3cc.chunk.js"
+  },
+  {
+    "revision": "b6b77396646600942777",
+    "url": "/static/js/1.b6b77396.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "161b6bb55b1b6f956b48188bb1ac6456",
+    "url": "/index.html"
+  }
+];
--- index.js
+++ index.js
@@ -1,9 +1,12 @@
 const express = require('express');
 const logger = require('morgan');
+const cors = require('cors');
 
 const app = express();
 app.use(express.json());
 app.use(logger('tiny'));
+app.use(express.static('build'));
+app.use(cors());
 
 let data = [
     { 
@@ -70,7 +73,7 @@ app.post('/api/persons', (request, response) => {
     response.json(newPerson)
 })
 
-const PORT = 3001;
+const PORT = process.env.PORT || 3001;
 app.listen(PORT, () => {
     console.log(`App running on port ${PORT}`)
 })
