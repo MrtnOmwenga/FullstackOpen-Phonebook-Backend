7a4e88a5c9d1447ebc65f08c2c1da794c5ee6c74
MrtnOmwenga <martin36449@gmail.com>
2023-07-19 02:44:59 +0300

    Excercises 3.15-3.18

--- index.js
+++ index.js
@@ -1,56 +1,66 @@
+require('dotenv').config();
 const express = require('express');
 const logger = require('morgan');
 const cors = require('cors');
+const Phonebook = require('./models/phonebook');
+const { ObjectId } = require('mongodb');
+const data = require('./data');
 
 const app = express();
+app.use(express.static('build'));
 app.use(express.json());
 app.use(logger('tiny'));
-app.use(express.static('build'));
 app.use(cors());
 
-let data = [
-    { 
-      "id": 1,
-      "name": "Arto Hellas", 
-      "number": "040-123456"
-    },
-    { 
-      "id": 2,
-      "name": "Ada Lovelace", 
-      "number": "39-44-5323523"
-    },
-    { 
-      "id": 3,
-      "name": "Dan Abramov", 
-      "number": "12-43-234345"
-    },
-    { 
-      "id": 4,
-      "name": "Mary Poppendieck", 
-      "number": "39-23-6423122"
+
+app.get('/api/persons', (request, response, next) => {
+    if (process.env.STORAGE_TYPE === 'FILE') {
+        response.json(data);
+    }else {
+        Phonebook.find({}).then(people => {
+            response.json(people);
+        }).catch(error => next(error));
     }
-]
-
-app.get('/api/persons', (request, response) => {
-    response.json(data);
 })
 
-app.get('/api/persons/:id', (request, response) => {
-    const id = Number(request.params.id);
-    const person = data.find(person => person.id === id);
+app.get('/api/persons/:id', (request, response, next) => {
+    if (process.env.STORAGE_TYPE === 'FILE') {
+        const id = Number(request.params.id);
+        const person = data.find(person => person.id === id);
 
-    person ? response.json(person) : response.status(404).end();
+        person ? response.json(person) : response.status(404).end();
+    }else {
+        Phonebook.findById(request.params.id).then(person => {
+            if (person){
+                response.send(person);
+            }else {
+                response.status(404).end();
+            }
+        }).catch(error => next(error));
+    }
 })
 
-app.get('/info', (request, response) => {
-    response.send(`<p> Phonebook has info for ${data.length} people <br> ${new Date()}</p>`);
+app.get('/info', (request, response, next) => {
+    if (process.env.STORAGE_TYPE === 'FILE') {
+        response.send(`<p> Phonebook has info for ${data.length} people <br> ${new Date()}</p>`);
+    }else {
+        Phonebook.count().then(number => {
+            response.send(`<p> Phonebook has info for ${number} people <br> ${new Date()}</p>`);
+        }).catch(error => next(error));
+    }
 })
 
-app.delete('/api/persons/:id', (request, response) => {
-    const id = Number(request.params.id);
-    data = data.filter(person => person.id !== id);
+app.delete('/api/persons/:id', (request, response, next) => {
+    if (process.env.STORAGE_TYPE === 'FILE') {
+        const id = Number(request.params.id);
+        data = data.filter(person => person.id !== id);
 
-    response.status(204).end();
+        response.status(204).end();
+    }else {
+        Phonebook.deleteOne({_id: new ObjectId(request.params.id)})
+        .then(response.status(204).end())
+        .catch(error => next(error));
+    }
 })
 
 logger.token('body', req => {
@@ -62,16 +72,36 @@ app.post('/api/persons', (request, response) => {
     if (!request.body.name){
         return response.status(422).send(`missing name`);
     };
-    if (data.map(person => person.name).includes(request.body.name)){
-        return response.status(409).send(`name must be unique`);
-    };
-    const newPerson = {
-        ...request.body,
-        id: Math.floor(Math.random() * 100)
-    };
-    data.push(newPerson)
-    response.json(newPerson)
+
+    if (process.env.STORAGE_TYPE === 'FILE') {
+        if (data.map(person => person.name).includes(request.body.name)){
+            return response.status(409).send(`name must be unique`);
+        };
+
+        const newPerson = {
+            ...request.body,
+            id: Math.floor(Math.random() * 100)
+        };
+
+        data.push(newPerson);
+        response.json(newPerson);
+    }else {
+        const person = new Phonebook(request.body);
+        person.save().then(savedPerson => response.json(savedPerson));
+    }
 })
+
+const errorHandler = (error, request, response, next) => {
+    console.error(error.message)
+  
+    if (error.name === 'CastError') {
+      return response.status(400).send({ error: 'malformatted id' })
+    } 
+  
+    next(error)
+  }
+  
+app.use(errorHandler)
 
 const PORT = process.env.PORT || 3001;
 app.listen(PORT, () => {
